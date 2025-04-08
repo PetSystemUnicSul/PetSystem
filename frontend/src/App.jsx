7fffb7c2bc1179c8fa5a17573eca706a600dc520
Rebecca <rss_ramos@hotmail.com>
2025-04-08 17:41:30 -0300

    Ajustes header e home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
-import Header from "./components/header";
 import Login from "./pages/login";
 import Cadastro from "./pages/cadastro";
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login/>} />
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,28 +1,56 @@
 import React from "react";
 import "../styles/home.css";
 import cachorro from "../assets/images/cachorro.jpg";
+import Header from "../components/header";
 
 function Home() {
   return (
     <>
+      <Header />
       <div className="main">
-        <div className="intro">
+        <section className="intro">
           <img src={cachorro} className="intro-img" />
-          <h1 className="title">Titulo</h1>
+          <h1 className="title">Bem-vindo a PetSystem</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
-            excepturi, doloribus eum nam aperiam, beatae porro ratione voluptate
-            sunt placeat ab perspiciatis vitae eligendi? Officiis maxime omnis
-            ipsa mollitia saepe!
+            Onde cuidar do seu pet nunca foi tão fácil. Nosso objetivo é
+            simplificar a vida dos tutores oferecendo uma plataforma completa
+            para gerenciar todos os aspectos da saúde e bem-estar dos seus
+            animais de estimação. Desde o cadastro detalhado de pets e o
+            agendamento de consultas até o controle de vacinas e vermífugos,
+            estamos aqui para proporcionar organização e tranquilidade para você
+            e seus companheiros peludos.
           </p>
-          <h2 className="title">Conheça a PetSystem</h2>
-          <div className="intro-display">
-            <img src={cachorro} alt="" />
-            <img src={cachorro} alt="" />
-            <img src={cachorro} alt="" />
-            <img src={cachorro} alt="" />
+        </section>
+
+        <section className="funcionalidades">
+          <h1 className="title">Funcionalidades</h1>
+          <div className="funcionalidades-container">
+            <div className="func">
+              <div className="icon">
+                <img src={cachorro} alt="" className="func-icon" />
+              </div>
+              <div className="text">
+                <h2 className="func-title">Cadastro de Pets</h2>
+                <span>
+                  Registre e atualize informações dos pets, como nome, raça e
+                  idade.
+                </span>
+              </div>
+            </div>
+            <div className="func">
+              <div className="icon">
+                <img src={cachorro} alt="" className="func-icon" />
+              </div>
+              <div className="text">
+                <h2 className="func-title">Cadastro de Consultas</h2>
+                <span>
+                  Agende e acompanhe consultas veterinárias com histórico de
+                  diagnósticos.
+                </span>
+              </div>
+            </div>
           </div>
-        </div>
+        </section>
       </div>
     </>
   );

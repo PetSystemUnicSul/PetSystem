14855067e1bbc3bf0ae2084a00539f98095b4423
Rebecca <rss_ramos@hotmail.com>
2025-04-08 13:22:15 -0300

    Adicionar introdução da Home

--- frontend/src/assets/componentes/header.jsx
+++ frontend/src/assets/componentes/header.jsx
@@ -4,15 +4,15 @@ import logo from "../images/Logo.png";
 
 function Header() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src={logo} />
-      </div>
-      <div className="header-buttons">
-        <div className="header-btn">Login</div>
-        <div className="header-btn">Cadastrar</div>
-      </div>
-    </header>
+      <header className="header">
+        <div className="logo">
+          <img src={logo} />
+        </div>
+        <div className="header-buttons">
+          <div className="header-btn">Login</div>
+          <div className="header-btn">Cadastrar</div>
+        </div>
+      </header>
   );
 }
 

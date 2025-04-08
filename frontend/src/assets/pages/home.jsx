56707bd2b87f0e8df76c97dcc8072fabf1f8e980
Rebecca <rss_ramos@hotmail.com>
2025-04-07 22:25:15 -0300

    Atualiza Home e App.jsx, adiciona componente Header e pasta de imagens.

---
+++ frontend/src/assets/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/home.css';
+import Header from '../componentes/header.jsx';
+
+function Home() {
+  return (
+    <> 
+      <Header/>
+    </>
+  );
+}
+
+export default Home;

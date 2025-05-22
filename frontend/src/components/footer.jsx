610a7836ec2f1d3908ac24a4aa5549c03649a623
Rebecca <rss_ramos@hotmail.com>
2025-05-22 18:59:44 -0300

    Home: Adicionado link para redes sociais

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,20 +1,23 @@
-import '../styles/footer.css';
-import { Instagram, Twitter, Youtube } from 'lucide-react';
+import "../styles/footer.css";
+import { Instagram, Twitter, Youtube } from "lucide-react";
 
 function Footer() {
-    return (
-        <footer>
-            <div className="div-redes-sociais">
-            <Instagram className='icon-redes-sociais'/>
-            <Twitter className='icon-redes-sociais'/>
-            <Youtube className='icon-redes-sociais'/>
-            </div>
-            <div className="div-informacoes">
-                <p>Termos de Licença e Política de Privacidade</p>
-                <p>Todos os direitos reservados para &copy;PetSystem</p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="div-redes-sociais">
+        <a href="https://www.instagram.com/petsystem.app/">
+          <Instagram className="icon-redes-sociais" />
+        </a>
+
+        <Twitter className="icon-redes-sociais" />
+        <Youtube className="icon-redes-sociais" />
+      </div>
+      <div className="div-informacoes">
+        <p>Termos de Licença e Política de Privacidade</p>
+        <p>Todos os direitos reservados para &copy;PetSystem</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
